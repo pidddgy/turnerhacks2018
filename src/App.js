baa89c053ff4cbd208ae3fdf5dacb6c470428978
Marcus Chong <marcus.jn.chong@gmail.com>
2018-06-03 11:25:46 -0400

    put map to one side and put text on right side

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 
 import './App.css';
+
 import Map from './components/Map.js'
+import RightSideStuff from './components/RightSideStuff.js';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Map/>
-        
+        <RightSideStuff/>
       </div>
     );
   }
---
+++ src/components/RightSideStuff.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/RightSideStuff.css';
+
+const RightSideStuff = (props) => {
+    return(
+      <div className = 'bigwrapper'>
+        <h1> aaa </h1>
+      </div>
+    )
+}
+
+export default RightSideStuff;
