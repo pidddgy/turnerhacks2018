ef72dce17df0cd8d058798be1c03ff9ba4d1572a
Marcus Chong <marcus.jn.chong@gmail.com>
2018-06-03 12:42:24 -0400

    theres hexagons now

--- src/components/Map.js
+++ src/components/Map.js
@@ -4,7 +4,7 @@ import '../css/Map.css'
 const Map = (props) => {
     return(
       <div class="embed-container">
-        <iframe width="250" height="200" frameborder="0" scrolling="yes" marginheight="0" marginwidth="0" title="sasdfsa" src="//marcus-chong.maps.arcgis.com/apps/Embed/index.html?webmap=683d63bdd05e45e8b53bac2274e01869&extent=-80.1838,43.5941,-79.2541,43.8408&zoom=true&previewImage=false&scale=true&disable_scroll=true&theme=light"></iframe>
+        <iframe width="500" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" title="asfsdfdsfdsf" src="//marcus-chong.maps.arcgis.com/apps/Embed/index.html?webmap=769f1a0f67b14839bb684f64ded48d39&extent=-79.8472,43.6499,-79.6677,43.7116&zoom=true&previewImage=false&scale=true&disable_scroll=true&theme=light"></iframe>
       </div>
     )
 }
